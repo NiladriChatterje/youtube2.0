74f4c87ef3dbb7a8b1c4176f62b1e48491d81faa
NiladriChatterje <radongtxgpu@gmail.com>
2022-10-07 01:24:22 +0530

    second

--- src/index.jsx
+++ src/index.jsx
@@ -6,11 +6,10 @@ import {BrowserRouter} from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-  <React.StrictMode>
+  
     <BrowserRouter>
     <ChakraProvider>
     <App />
     </ChakraProvider>
     </BrowserRouter>
-  </React.StrictMode>
 )
