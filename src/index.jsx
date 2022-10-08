1ac913b6b2a60bd01300bd671c47c3d72d526b50
NiladriChatterje <radongtxgpu@gmail.com>
2022-10-09 00:51:49 +0530

    fifth

--- src/index.jsx
+++ src/index.jsx
@@ -7,9 +7,11 @@ import {BrowserRouter} from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
   
   
-    <BrowserRouter>
+    
     <ChakraProvider>
+    <BrowserRouter>
     <App />
-    </ChakraProvider>
     </BrowserRouter>
+    </ChakraProvider>
+    
 )
